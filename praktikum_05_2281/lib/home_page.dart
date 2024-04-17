23be9869800f9452398c18f731472eddeb7ac9f1
ratrisap <21102281@ittelkom-pwt.ac.id>
2024-04-17 22:24:22 +0700

    saving_app

--- praktikum_05_2281/lib/home_page.dart
+++ praktikum_05_2281/lib/home_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:praktikum_05_2281/second_page.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
